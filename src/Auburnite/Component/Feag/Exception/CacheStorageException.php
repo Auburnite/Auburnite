e0fc3cb91bf367a04c9b3e474e33adea85d99579
Jordan Wamser <jwamser@redpandacoding.com>
2024-03-14 07:41:14 -0500

    [Feag] Initial setup

--- src/Auburnite/Component/Feag/Exception/CacheStorageException.php
+++ src/Auburnite/Component/Feag/Exception/CacheStorageException.php
@@ -1,8 +1,18 @@
 <?php
 
+/*******************************************************************************
+ * Auburnite
+ *
+ * @link                https://github.com/Auburnite/Auburnite
+ * @copywrite           Copywrite (c) 2023-present | Jordan Wamser - RedPanda Coding
+ * @license             https://github.com/Auburnite/Auburnite/blob/main/LICENSE
+ ******************************************************************************/
 namespace Auburnite\Component\Feag\Exception;
 
-class CacheStorageException
+use Auburnite\Auburnite\Component\Exception\Exception;
+use Psr\SimpleCache\CacheException;
+
+class CacheStorageException extends Exception implements CacheException
 {
 
 }
