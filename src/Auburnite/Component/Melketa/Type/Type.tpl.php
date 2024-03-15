d353c99da9e5bc2cb13401f1456cc714a7e3c901
Jordan Wamser <jwamser@redpandacoding.com>
2024-03-15 10:27:58 -0500

    syntax corrections

--- src/Auburnite/Component/Melketa/Type/Type.tpl.php
+++ src/Auburnite/Component/Melketa/Type/Type.tpl.php
@@ -1,23 +1,23 @@
-<?= "<?php\n" ?>
+<?php echo "<?php\n"; ?>
 
-namespace <?= $namespace ?>;
+namespace <?php echo $namespace; ?>;
 
-<?= $use_statemnts; ?>
+<?php echo $use_statemnts; ?>
 
-class <?= $class_name ?> extends <?= $base_class_name."\n" ?>
+class <?php echo $class_name; ?> extends <?php echo $base_class_name."\n"; ?>
 {
 
     public function getSQLDeclaration(array $column, AbstractPlatform $platform)
     {
         // right here is where we need to dynamically setup the column data
 
-        $column = <?= $column_settings ?>;
+        $column = <?php echo $column_settings; ?>;
 
         return parent::getSQLDeclaration($column, $platform);
     }
 
     public function getName()
     {
-        return '<?= $name ?>';
+        return '<?php echo $name; ?>';
     }
 }
